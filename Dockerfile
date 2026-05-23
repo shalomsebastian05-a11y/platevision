07dac245eebfd2eb9f15d40375e467518e4cec7e
Ubuntu <ubuntu@ip-10-0-136-51.ap-south-1.compute.internal>
2026-05-23 04:23:53 +0000

    Added RDS, Docker, security, and CI/CD

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,19 @@ RUN pip install flask flask-bcrypt flask-sqlalchemy Pillow python-dotenv gunicor
 COPY . .
 EXPOSE 5000
 CMD ["python3", "app.py"]
+
+
+
+FROM python:3.11-slim
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+EXPOSE 5000
+
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -8,6 +8,12 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+@app.after_request
+def add_security_headers(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    response.headers['X-XSS-Protection'] = '1; mode=block'
+    return response
 app.secret_key = 'mysecretkey123'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
